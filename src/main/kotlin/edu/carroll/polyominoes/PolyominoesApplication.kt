58da5656ba09ef441f1d9d82a401f3ba401a542e
Jaden Bathon <jbathon@carroll.edu>
2023-02-01 14:26:39 -0700

    Initial Commit

---
+++ src/main/kotlin/edu/carroll/polyominoes/PolyominoesApplication.kt
@@ -0,0 +1,11 @@
+package edu.carroll.polyominoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolyominoesApplication
+
+fun main(args: Array<String>) {
+	runApplication<PolyominoesApplication>(*args)
+}
---
+++ src/test/kotlin/edu/carroll/polyominoes/PolyominoesApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.carroll.polyominoes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PolyominoesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
