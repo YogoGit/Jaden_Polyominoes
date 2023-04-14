547917164285aae56b7de02557e852404ba47ae1
Jaden Bathon <jbathon@carroll.edu>
2023-04-14 12:22:43 -0600

    Modified docker-compose yaml to fix errors

--- docker/polyominoes/postgres/primary/setup-primary.sh
+++ docker/polyominoes/postgres/primary/setup-primary.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 echo "host replication all 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
+echo "host all postgres polyominoes.application_cluster trust" >> "$PGDATA/pg_hba.conf"
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-CREATE DATABASE $POSTGRES_DB;
+CREATE USER $POSTGRES_APP_USER WITH ENCRYPTED PASSWORD '$POSTGRES_APP_PASSWORD';
+GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB to $POSTGRES_APP_USER;
 EOSQL
 cat >> ${PGDATA}/postgresql.conf <<EOF
-wal_level = replica
-archive_mode = on
-archive_command = 'cd .'
-max_wal_senders = 8
-hot_standby = on
 EOF
